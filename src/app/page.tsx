0a38f6be96f031b4beeee43febd4d77a93501420
I'm Jaystar <jbayilara@gmail.com>
2025-10-17 19:08:17 +0100

    updated auth page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
+import FormSection from "@/components/ui/login/form-section";
 import styles from "./page.module.scss";
 import ImageSection from "@/components/ui/login/image-section";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <div className={styles.container}>
+        <div className={styles["logo-cover"]}>
+          <div className={styles["logo-cover--image"]}>
+            <Image src="/icons/logo.svg" alt="logo" width={173} height={36} />
+          </div>
+        </div>
         <div className={styles.imageSection + " screen-height"}>
           <ImageSection />
         </div>
 
-        <div className={styles.form}>
-          <h1>Log in</h1>
+        <div className={styles.form + " screen-height"}>
+          <FormSection />
         </div>
       </div>
     </main>
---
+++ src/components/ui/login/form-section.tsx
@@ -0,0 +1,108 @@
+"use client";
+import React from "react";
+import styles from "./form-section.module.scss";
+
+import { useForm } from "@mantine/form";
+import { loginSchema } from "@/app/schemas/auth.schema";
+import { Button, TextInput } from "@mantine/core";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+const inputStyle = {
+  input: {
+    height: 50,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: "rgba(84, 95, 125, 0.15)",
+    fontSize: 14,
+    "::placeholder": {
+      color: "rgba(84, 95, 125, 0.15)",
+    },
+  },
+};
+
+const FormSection = () => {
+  // GENERAL VARIABLES
+  const router = useRouter();
+  // FORM
+  const [showPassword, setShowPassword] = React.useState(false);
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate: (values) => {
+      const result = loginSchema.safeParse(values);
+      if (!result.success) {
+        return result.error.flatten().fieldErrors;
+      }
+      return {};
+    },
+  });
+
+  function handleSubmit(values: typeof form.values) {
+    console.log(values);
+    router.push("/dashboard/users");
+  }
+
+  return (
+    <div className={styles.formCover}>
+      <div className={styles.header}>
+        <h2>Welcome!</h2>
+        <small>Enter details to login.</small>
+      </div>
+      <form
+        className={styles.form}
+        onSubmit={form.onSubmit((values) => handleSubmit(values))}
+      >
+        <TextInput
+          placeholder="Email"
+          key={form.key("email")}
+          styles={inputStyle}
+          {...form.getInputProps("email")}
+        />
+        <TextInput
+          mt="lg"
+          placeholder="Password"
+          type={showPassword ? "text" : "password"}
+          key={form.key("password")}
+          styles={{...inputStyle,}}
+          {...form.getInputProps("password")}
+          rightSectionWidth={50}
+          rightSection={
+            <Button
+              variant="transparent"
+              className={styles["show-password-button"]}
+              size="xs"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? "Hide" : "Show"}
+            </Button>
+          }
+        />
+
+        <div className={styles.forgot}>
+          <Link
+            href="#"
+            rel="noopener noreferrer"
+            className={styles["forgot-link"]}
+          >
+            Forgot PASSWORD?
+          </Link>
+        </div>
+
+        <Button
+          type="submit"
+          size="md"
+          className={styles.button}
+          aria-label="Login"
+        >
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default FormSection;
